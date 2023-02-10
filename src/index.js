01e894f63a669ca619e707424aa1422ded7ef7ea
Rodion <mrDalmatov@MacBook-Pro-Rodion.local>
2023-02-10 18:38:40 +0700

    done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+  return Math.abs(n >= 0 
+    ? +`${n}`.split('').reverse().join('') 
+    : -(`${-n}`.split('').reverse().join('')));
+
+    
+};
+
+
